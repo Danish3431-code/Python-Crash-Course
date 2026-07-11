21d28d7b3988b1382e6f6dc47a025cb02ffc30a6
Danish Nadeem <0364bhabscsias20@gmail.com>
2026-07-11 18:20:44 +0500

    Add files via upload

---
+++ Module Package lib/module.py
@@ -0,0 +1,44 @@
+# Modules in Python
+
+# single py file
+
+# create a module mymodule.py file
+
+#use mymodule file
+
+# import mymodule
+# mymodule.say_hello("Danish")
+
+# mymodule.say_bye("Ahmed")
+
+#import /use specific part of code
+#jab bi file ka endar ik specific code ko execute kerna ho tou hum esa likhta han
+
+# from mymodule import person1
+# print(person1["age"])
+
+
+
+#package: collection of modules/py files+ __init__file
+
+#library: collection of modules and packages
+
+#in python there are some in_built libraries you can search it  from google  and check what are those libraries.
+
+# import math 
+
+# A=16
+# print(math.sqrt(A)) # 4.0 
+
+# #import specific function from the math library
+
+# from math import factorial
+# B=4
+# print(factorial(B)) # 24
+
+
+#install new modules/lib from your computer terminal run below instraction.
+#pip install <library_name>
+
+# import pandas as pd
+
---
+++ Module Package lib/mymodule.py
@@ -0,0 +1,14 @@
+# create a code to use in another py file.
+
+# def say_hello(name):
+#     return print(f"Hello, {name}, kaise ho")
+
+
+# def say_bye(name):
+#     return print(f"Bye, {name}, takecare")
+
+
+person1={
+    "name":"Ali",
+    "age":24
+}
